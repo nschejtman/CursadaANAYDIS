e4b6b34eaabcda04ce197d7d3f98366bd87f49d0
Nicolas Schejtman <nschejtman@hotmail.com>
2013-09-19 14:46:11 -0300

    Added SorterProviderImpl and deleted some sorters.

---
+++ src/main/java/anaydis/sort/SorterProviderImpl.java
@@ -0,0 +1,40 @@
+package anaydis.sort;
+
+import anaydis.sort.provider.SorterProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nicolas
+ * Date: 19/09/13
+ * Time: 14:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class SorterProviderImpl implements SorterProvider {
+
+    private final Map<SorterType, Sorter> sorters;
+
+    public SorterProviderImpl() {
+        sorters = new EnumMap<SorterType, Sorter>(SorterType.class);
+        addSorter(new InsertionSorter());
+    }
+
+    private void addSorter(Sorter sorter) {
+        sorters.put(sorter.getType(), sorter);
+    }
+
+    @NotNull
+    @Override
+    public Sorter getSorterForType(@NotNull SorterType type) {
+        return sorters.get(type);
+    }
+
+    @NotNull
+    @Override
+    public Iterable<Sorter> getAllSorters() {
+        return sorters.values();
+    }
+}
---
+++ src/main/java/anaydis/sort/animation/AnimationMain.java
@@ -0,0 +1,14 @@
+package anaydis.sort.animation;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nicolas
+ * Date: 19/09/13
+ * Time: 14:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnimationMain {
+    public static void main(String[] args) {
+
+    }
+}
