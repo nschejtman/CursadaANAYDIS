4605fbe3cd63ec363cdac17075eb76e8fba732df
Nicolas Schejtman <nschejtman@hotmail.com>
2013-10-18 09:36:10 -0300

    Modified range & nth

--- src/main/scala/anaydis/search/AbstractTreeMap.scala
+++ src/main/scala/anaydis/search/AbstractTreeMap.scala
@@ -2,13 +2,42 @@ package anaydis.search
 
 import scala.collection.immutable.Stack
 import scala.collection.mutable.LinkedList
+import scala.collection.mutable
 
 trait AbstractTreeMap[K, V] extends MyMap[K, V] {
 
   case class Node(key: K, var value: V, var left: Node = null, var right: Node = null) {
     override def toString: String = s"k $key, v $value, l:$left r:$right"
 
-    def size: Int = 1 + left.size + right.size
+    def element = value
+
+
+    def size: Int = 1 + {
+      if (left == null) 0 else left.size
+    } + {
+      if (right == null) 0 else right.size
+    }
+
+    def nth(n: Int): Node = {
+      val idx = if(left != null) left.size else 0
+      if (n == idx) this
+      else if (n <= idx && left != null) left.nth(n)
+      else if (n >= idx && right != null) right.nth(n - idx - 1)
+      else throw new IndexOutOfBoundsException
+    }
+
+    def range(lo: Int, hi: Int): List[Node] = {
+      if (lo > hi) throw new IllegalArgumentException
+      val idx = size
+      if (lo > idx || hi > idx) throw new IllegalArgumentException
+      var list = List[Node]()
+
+      for (x <- lo to hi) {
+        list = list ++ List(nth(x))
+      }
+
+      list
+    }
   }
 
   protected var head: Node = null
@@ -22,7 +51,6 @@ trait AbstractTreeMap[K, V] extends MyMap[K, V] {
   }
 
   def newLeaf(k: K, v: V) = {
-    size += 1;
     Node(k, v)
   }
 
@@ -44,35 +72,12 @@ trait AbstractTreeMap[K, V] extends MyMap[K, V] {
   def firstNode(n: Node): Node = if (n == null || n.left == null) n else firstNode(n.left)
 
   def nth(n: Int): Node = {
-    val stack = toStack(head)
-    for (x <- 0 to n) {
-      stack.pop
-    }
-    stack.top
+    head.nth(n)
   }
 
-  def range(lo: Int, hi: Int): LinkedList[Node] = {
-    val list = new LinkedList[Node]
-    val stack = toStack(head)
-    for (i <- 0 to lo - 1) {
-      stack.pop
-    }
-    for (i <- lo to hi) {
-      list -> stack.top
-      stack.pop
-    }
-    list
+  def range(lo: Int, hi: Int): List[Node] = {
+    head.range(lo, hi)
   }
-
-
-  def toStack(node: Node): Stack[Node] = {
-    val stack = new Stack[Node]
-    stack.pushAll(toStack(node.right))
-    stack.push(node)
-    stack.pushAll(toStack(node.left))
-    stack
-  }
-
 
   override def toString: String = if (head == null) "{}" else head.toString()
 
--- src/main/scala/anaydis/search/Main.scala
+++ src/main/scala/anaydis/search/Main.scala
@@ -4,7 +4,7 @@ object Main extends App {
   var map: TreeMap[Int, String] = new TreeMap[Int, String]
   map.put(5, "Pepe")
   map.put(2, "Pepo")
-  map.put(2, "Pepa")
-  println(map)
+  map.put(7, "Pepa")
+  print(map.range(0,1))
 
 }
